a68e6469b5a1e530dec7f5ed63af98e9dbcf561e
Onur Özdemir <onurozdemir94@hotmail.com>
2020-11-29 21:24:05 +0300

    Update create_tables.py
    add docstring

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,44 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+    """ 
+    This function, clear all table with given drop queries on sql_queries.py. 
+    
+    Args:
+        cur (int): Cursor of AWS Redshift Cluster
+        conn (str): Conenction of AWS Redshift Cluster
 
+    """
+    try:
+        for query in drop_table_queries:
+            cur.execute(query)
+            conn.commit()
+    except Exception as e:
+        raise e
 
 def create_tables(cur, conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+    """ 
+    This function, create all table with given drop queries on sql_queries.py.
+    These tables for ETL process. If taken any error this step you musn't run etl.py 
+    script.
+    
+    Args:
+        cur (int): Cursor of AWS Redshift Cluster
+        conn (str): Conenction of AWS Redshift Cluster
 
+    """
+    try:
+        for query in create_table_queries:
+            cur.execute(query)
+            conn.commit()
+    except Exception as e:
+        raise e        
 
 def main():
+    """ 
+    This script create new tables for ETL process. Firstly, if tables exists delete them.
+    Secondly, create new empty tables for fresh ETL process.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
